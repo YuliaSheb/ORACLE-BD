813466f241d698d5cb83da2f157e809e18405ad9
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2023-02-24 01:31:27 +0300

    add 1-2 tasks

--- LAB1/script.sql
+++ LAB1/script.sql
@@ -1,1 +1,19 @@
+CREATE TABLE myTable
+(
+ id number PRIMARY KEY,
+ val number NOT NULL 
+);
 
+DECLARE
+    peremen number := 0;
+BEGIN 
+   WHILE peremen <= 10000
+   LOOP
+    peremen := peremen + 1;
+    INSERT INTO myTable VALUES (peremen, ROUND(dbms_random.value(1,10000)));
+   END LOOP;
+END;
+
+SELECT * FROM myTable;
+
+DROP TABLE myTable;
