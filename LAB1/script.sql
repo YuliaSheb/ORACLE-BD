3af9f2aec75f004f84700305bdb7815b9c3870a6
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2023-02-24 02:05:14 +0300

    add task 3

--- LAB1/script.sql
+++ LAB1/script.sql
@@ -14,6 +14,23 @@ BEGIN
    END LOOP;
 END;
 
+CREATE OR REPLACE FUNCTION even_odd  RETURN VARCHAR IS 
+    even number(10);
+    odd number(10);
+    result VARCHAR(10); 
+BEGIN
+    SELECT count(*) INTO even FROM myTable WHERE mod(val,2)=0;
+    SELECT count(*) INTO odd FROM myTable WHERE mod(val,2)!=0;
+    IF even>odd THEN RETURN 'TRUE';
+    ELSIF even<odd THEN RETURN 'FALSE';
+    ELSE RETURN 'EQUAL';
+    END IF;
+    dbms_output.put_line(odd);
+    dbms_output.put_line(even);
+    
+    RETURN result;
+END;
+
 SELECT * FROM myTable;
 
 DROP TABLE myTable;
