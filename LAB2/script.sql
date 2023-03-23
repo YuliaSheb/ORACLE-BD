5cba1e6e075afdefcd26731d8d062109f03c1f96
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2023-03-23 19:05:42 +0300

    add 2 task

--- LAB2/script.sql
+++ LAB2/script.sql
@@ -21,3 +21,21 @@ INSERT INTO Students(id,name,group_id) VALUES (8, 'Denis', 3);
 INSERT INTO Students(id,name,group_id) VALUES (9, 'Arseniy', 3);
 INSERT INTO Students(id,name,group_id) VALUES (10, 'Roma', 4);
 INSERT INTO Students(id,name,group_id) VALUES (11, 'Oleg', 5);
+
+INSERT INTO groupps(id,name,c_val) VALUES (1, '053501', 3);
+INSERT INTO groupps(id,name,c_val) VALUES (2, '053502', 3);
+INSERT INTO groupps(id,name,c_val) VALUES (3, '053503', 3);
+INSERT INTO groupps(id,name,c_val) VALUES (4, '053504', 1);
+INSERT INTO groupps(id,name,c_val) VALUES (5, '053505', 1);
+
+CREATE OR REPLACE TRIGGER uniqueIdStudents
+BEFORE INSERT OR UPDATE OF id ON student
+FOR EACH ROW
+DECLARE 
+    n_id NUMBER;
+BEGIN
+    SELECT count(*) INTO n_id FROM student WHERE student.id =: new.id;
+    IF n_id > 0 THEN raise_application_error(-20000, 'It is not unique id');
+    ELSE dbms_output.put_line(' Succes');
+    END IF;
+END;
