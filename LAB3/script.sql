dabecf00de86d10d6a2e3dcca918d35f004cbfb3
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2023-04-15 14:05:41 +0300

    add tables

--- LAB3/script.sql
+++ LAB3/script.sql
@@ -5,3 +5,32 @@ CREATE USER c##pod IDENTIFIED BY passpod;
 GRANT ALL PRIVILEGES TO c##dev;
 
 GRANT ALL PRIVILEGES TO c##pod;
+
+CREATE TABLE c##dev.animals (
+    id  NUMBER NOT NULL,
+    name VARCHAR(20),
+    group_live NUMBER PRIMARY KEY
+    );
+    
+CREATE TABLE c##dev.forests (
+    id  NUMBER NOT NULL,
+    name VARCHAR(50),
+    CONSTRAINT live FOREIGN KEY (id) REFERENCES c##dev.animals(group_live)
+    );
+    
+CREATE TABLE c##dev.plants (
+    id  NUMBER NOT NULL,
+    name VARCHAR(50)
+    );
+    
+CREATE TABLE c##pod.animals (
+    id  NUMBER NOT NULL,
+    name VARCHAR(20),
+    group_live NUMBER PRIMARY KEY
+    );
+    
+CREATE TABLE c##pod.zoo (
+    id  NUMBER NOT NULL,
+    name VARCHAR(50),
+    CONSTRAINT live FOREIGN KEY (id) REFERENCES c##pod.animals(group_live)
+    );
